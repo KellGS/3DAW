39c64e372d7124a9daf62252d7528b4a2d1c06ef
Kelly Soares <58700804+KellGS@users.noreply.github.com>
2020-08-18 09:46:21 -0300

    Add files via upload

---
+++ calculeIdade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+    $texto = "Kelly";
+    $data_nasc = "02/04/1998";
+    $idade = 2020 - 1998;
+?>
+
+<?php echo "Meu nome é $texto" ?>
+<br>
+<?php echo "Nasci em $data_nasc" ?>
+<br>
+<?php echo "Tenho $idade anos" ?>
+
+</body>
+</html>
