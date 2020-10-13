46ecc8cd1bf54d4e5add57795bd2bc64841b0a50
Kelly Soares <58700804+KellGS@users.noreply.github.com>
2020-10-13 20:14:33 -0300

    Add files via upload

---
+++ AV1/connection.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "3daw";
+$password = "mysql123";
+$dbname = "3dawav1";
+
+// Create connection
+$con = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($con->connect_error) {
+  die("Connection failed: " . $con->connect_error);
+}
+?>
